f7e0098cad4eebffa641efb611556e09c4ffaaf7
Aymane Atigui <aymane.atigui.0202@gmail.com>
2023-03-02 18:41:25 +0100

    Back to originale

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Form } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { Profile } from './pages/Profile';
-import { Contact } from './pages/Contact';
-import { Nav } from './pages/Nav';
-import { Forme } from './pages/Forme';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
 
-  const client = new QueryClient();
 
   return (
     <div className="App">
-      <QueryClientProvider client={client}>
-          <Router>
-            <Nav />
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/Forme' element={<Forme />} />
-              <Route path='/profile' element={<Profile />} />
-              <Route path='/contact' element={<Contact />} />
-              <Route path='*' element={<h1>Page Not found</h1>} />
-            </Routes>
-          </Router>
-          </QueryClientProvider>
+      
     </div>
   );
 }
--- src/pages/Profile.js
+++
@@ -1,5 +0,0 @@
-import { ChangeProfile } from "../compenent/ChangeProfile";
-
-export const Profile = (props) =>{
-    return <h1>Profile, <ChangeProfile/></h1>
-};
